5bd21eb4b4544f557af26ffd5acde580b6feb3bd
Pierre Zemb <pierre.zemb.isen@gmail.com>
2013-10-21 14:49:28 +0200

    adding index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once("setup/config.php");
+require_once("lib/limonade.php");
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="stylesheet" href="static/login.css" />
+        <title>ISEN - Documents de rentrée</title>
+    </head>
+ 
+    <body>
+    	<h2>Hello world !!</h2>
+    </body>
+    
+</html>
