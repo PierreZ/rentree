7cce54d2de3d11dfd61dc1fee7e3ad5fbc4d9a5e
Pierre Zemb <pierre.zemb.isen@gmail.com>
2013-12-03 17:40:24 +0100

    patching int

--- sql/rentree.sql
+++ sql/rentree.sql
@@ -5,7 +5,7 @@ CREATE DATABASE rentree CHARACTER SET utf8 collate utf8_bin;
 use rentree
 
 CREATE TABLE `eleve` (
-	`id` int() NOT NULL auto_increment,
+	`id` int NOT NULL auto_increment,
 	`email` varchar(256) character set utf8 collate utf8_bin NOT NULL,
 	`nom` varchar(256) character set utf8 collate utf8_bin NOT NULL,
 	`datenaissance` DATE  NOT NULL,
@@ -16,14 +16,14 @@ CREATE TABLE `eleve` (
 );
 
 CREATE TABLE `document` (
-	`id` int() NOT NULL auto_increment,
+	`id` int NOT NULL auto_increment,
 	`fichier` varchar(256) character set utf8 collate utf8_bin NOT NULL,
 	`nompromotion` varchar(256) character set utf8 collate utf8_bin REFERENCES promotion(nompromotion),
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `promotion` (
-	`id` int() NOT NULL auto_increment,
+	`id` int NOT NULL auto_increment,
 	`nompromotion` varchar(256) character set utf8 collate utf8_bin NOT NULL,
 	PRIMARY KEY (`id`)
 );
