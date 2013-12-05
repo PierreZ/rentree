2f91b66baedba58c60b32f39a4e1606264fcddda
Corentin Delcourt <codl@codl.fr>
2013-12-05 08:39:04 +0100

    db schema: remove unnecessary charset and collation definitions

--- sql/rentree.sql
+++ sql/rentree.sql
@@ -6,24 +6,24 @@ use rentree
 
 CREATE TABLE `eleve` (
 	`id` int NOT NULL auto_increment,
-	`email` varchar(256) character set utf8 collate utf8_bin NOT NULL,
-	`nom` varchar(256) character set utf8 collate utf8_bin NOT NULL,
-	`datenaissance` DATE  NOT NULL,
-	`emailparent` varchar(256) character set utf8 collate utf8_bin NOT NULL,
-	`telparent` varchar(256) character set utf8 collate utf8_bin NOT NULL,
-	`nomparent` varchar(256) character set utf8 collate utf8_bin NOT NULL,
+	`email` varchar(256) NOT NULL,
+	`nom` varchar(256) NOT NULL,
+	`datenaissance` DATE NOT NULL,
+	`emailparent` varchar(256) NOT NULL,
+	`telparent` varchar(256) NOT NULL,
+	`nomparent` varchar(256) NOT NULL,
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `document` (
 	`id` int NOT NULL auto_increment,
-	`fichier` varchar(256) character set utf8 collate utf8_bin NOT NULL,
-	`nompromotion` varchar(256) character set utf8 collate utf8_bin REFERENCES promotion(nompromotion),
+	`fichier` varchar(256) NOT NULL,
+	`nompromotion` varchar(256) REFERENCES promotion(nompromotion),
 	PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `promotion` (
 	`id` int NOT NULL auto_increment,
-	`nompromotion` varchar(256) character set utf8 collate utf8_bin NOT NULL,
+	`nompromotion` varchar(256) NOT NULL,
 	PRIMARY KEY (`id`)
 );
