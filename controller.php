0793579604e39298617cb843c0d931de658bb7e2
Corentin Delcourt <codl@codl.fr>
2013-12-05 12:14:35 +0100

    add controller

---
+++ controller.php
@@ -0,0 +1,31 @@
+<?php
+function generate_403($type="json", $error="Forbidden"){
+	header("HTTP/1.1 403 Forbidden");
+	if($type="json"){
+		header("Content-Type: application/json");
+		return json_encode(Array("error" => $error));
+	}
+}
+
+function generate_404($type="json", $error="Not Found"){
+	header("HTTP/1.1 404 Not Found");
+	if($type="json"){
+		header("Content-Type: application/json");
+		return json_encode(Array("error" => $error));
+	}
+}
+function get_eleve(){
+	if(!is_admin() && !is_self(params("id"))
+		return generate_403();
+
+	$e = Eleve.find($id=params("id"));
+	if(!$e)
+		return generate_404();
+
+	return $e.toJson();
+}
+
+function pong(){
+	return "pong";
+}
+?>
--- index.php
+++ index.php
@@ -1,17 +1,33 @@
 <?php
 require_once("lib/limonade.php");
-?>
+require_once("controller.php");
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <link rel="stylesheet" href="static/login.css" />
-        <title>ISEN - Documents de rentrée</title>
-    </head>
- 
-    <body>
-    	<h2>Hello world !!</h2>
-    </body>
-    
-</html>
+//dispatch("/", "serve_client_app");
+
+dispatch("/eleve/:id", "get_eleve");
+//dispatch_post("/eleve/", "post_eleve");
+//dispatch_put("/eleve/:id", "put_eleve");
+
+//dispatch("/eleves/", "get_eleves");
+
+//dispatch("/document/:id", "get_document");
+//dispatch_post("/document/", "post_document");
+//dispatch_put("/document/:id", "put_document");
+
+//dispatch("/document/:id.pdf", "download_document");
+
+//dispatch("/documents/", "get_documents");
+
+
+//dispatch_post("/session", "post_session");
+//dispatch_delete("/session/:admin_id", "delete_session");
+
+//dispatch("/admin/:id", "get_admin");
+//dispatch_post("/admin/", "post_admin");
+//dispatch_put("/admin/:id", "put_admin");
+//dispatch_delete("/admin/:id", "delete_admin");
+
+//dispatch("/admins/", "get_admins");
+
+dispatch("/ping/", "pong");
+?>
