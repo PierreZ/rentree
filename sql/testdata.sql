4b97b2c17cd467c0a29ea80412732f1f49cbe3ef
Corentin Delcourt <codl@codl.fr>
2013-12-05 20:42:04 +0100

    test data

---
+++ sql/testdata.sql
@@ -0,0 +1,9 @@
+USE rentree;
+
+TRUNCATE TABLE eleve;
+
+INSERT INTO eleve
+	(email, nom, datenaissance, emailparent, nomparent, telparent)
+	VALUES
+	("duchmoll@isen.fr", "Duchmoll", "1992-01-01", "duchmoll@hotmail.fr", "Duchmoll Senior", "0246810121")
+;
