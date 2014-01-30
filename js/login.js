12a2df96ac551770daec524a27b1129defc2fb30
Pierre Zemb <pierre.zemb.isen@gmail.com>
2014-01-30 01:49:13 +0100

    adding first ajax request (not working)

--- js/login.js
+++ js/login.js
@@ -1,29 +1,48 @@
-function login(e){
-    e.preventDefault();
-    var b = document.querySelector("button");
-    b.disabled = true;
-    b.classList.add("loading");
+$(document).ready(
+	function() {
+		initLogin();
+		function login(e){
+		e.preventDefault();
+		var b = document.querySelector("button");
+		b.disabled = true;
+		b.classList.add("loading");
 
-    document.activeElement.blur();
+		document.activeElement.blur();
+		var URL = window.location.protocol + "//" + window.location.host+"/session"
+		var $email=$('#email').val(),
+			$paswd=$('#password').val();
+		jQuery.ajax({
+			type: 'POST', 
+			url: URL,
+			data: {
+				email: $email, 
+				password: $paswd
+			}, 
+			success: function(data, textStatus, jqXHR) {
+				alert(data);
+				window.setTimeout(function(){document.body.classList.add("fade");}, 2000, false);
+				window.setTimeout(function(){
+					document.body.classList.remove("login");
+					document.body.innerHTML = document.querySelector("template.panes").innerHTML;
+					document.body.classList.add("step-1");
+					document.body.classList.add("panes");
+					window.setTimeout(function(){
+						document.body.classList.remove("fade");
+						}, 100, false);
+					initPanes();
+					},  3000, false);
+				},
+			error: function(jqXHR, textStatus, errorThrown) {
 
-    // send login and password, recieve session key, blah blah blah
+			}
+		});
 
-    window.setTimeout(function(){document.body.classList.add("fade");}, 2000, false);
-    window.setTimeout(function(){
-        document.body.classList.remove("login");
-        document.body.innerHTML = document.querySelector("template.panes").innerHTML;
-        document.body.classList.add("step-1");
-        document.body.classList.add("panes");
-        window.setTimeout(function(){
-            document.body.classList.remove("fade");
-        }, 100, false);
-        initPanes();
-    },  3000, false);
-
-    // this is where we load the next page
-}
-function initLogin(e){
-    var b = document.querySelector("body.login button");
-    b.disabled = false;
-    document.querySelector("body.login form").addEventListener("submit", login, false);
-}
+		// this is where we load the next page
+		}
+		function initLogin(e){
+			var b = document.querySelector("body.login button");
+			b.disabled = false;
+			document.querySelector("body.login form").addEventListener("submit", login, false);
+		}
+	}
+);
--- views/elements/login.php
+++ views/elements/login.php
@@ -7,7 +7,6 @@
 	<script src="lib/angular.min.js" type="text/javascript"></script>
         <script src="js/login.js"></script>
         <script src="js/panes.js"></script>
-        <script>window.onload = initLogin;</script>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     </head>
     <body class="login">
