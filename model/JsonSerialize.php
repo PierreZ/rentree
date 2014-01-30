ef333d5a0368d3123b3c8dc588332ff0ffc80430
Corentin Delcourt <codl@codl.fr>
2014-01-30 09:26:51 +0100

    fix: only reimplement JsonSerialize if it does not exist

--- model/JsonSerialize.php
+++ model/JsonSerialize.php
@@ -6,22 +6,10 @@
  * @license    GNU General Public License version 2 or later; see LICENSE
  */
 
-/**
- * JsonSerializable interface. This file should only be loaded on PHP < 5.4
- * It allows us to implement it in classes without requiring PHP 5.4
- *
- * @link   http://www.php.net/manual/en/jsonserializable.jsonserialize.php
- * @since  1.0
- */
-interface JsonSerializable
-{
-        /**
-         * Return data which should be serialized by json_encode().
-         *
-         * @return  mixed
-         *
-         * @since   1.0
-         */
-        public function jsonSerialize();
+if(!interface_exists('JsonSerializable')){
+	interface JsonSerializable
+	{
+		public function jsonSerialize();
+	}
 }
 ?>
