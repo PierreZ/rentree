50c6b2d16848798ccc53ebb61caceba5d01c3ba7
PierreZ <pierre.zemb.isen@gmail.com>
2014-02-12 00:03:54 +0100

    fetching promos and documents almost operational

--- js/user.js
+++ js/user.js
@@ -3,7 +3,16 @@ function EleveCtrl($scope, $rootScope, $http, $cookies, $location) {
 	$scope.bodyClass = "step-0 panes fade";
 	$scope.status = null;
 	$scope.error = null;
-	$scope.get_documents();
+	// Initialisation de la promo choisie
+	// Selected permet de savoir quel promo à été choisies(plus précisément son ID)
+	$scope.selected=0;
+	$scope.documents = {};	
+
+	// Initialisation des tableaux
+	$scope.promos=new Array();
+	$scope.documents=new Array();
+
+	// Récupération des éléments
 	$http({
 		method: "GET",
 		url: "eleve/" + $cookies.id_eleve
@@ -16,6 +25,37 @@ function EleveCtrl($scope, $rootScope, $http, $cookies, $location) {
 		$scope.bodyClass = "step-0 panes";
 	});
 
+	// Récupération des promos
+	$http({
+		method: "GET",
+		url: "promos/",
+		timeout: 5000
+	}).then(function success(resp){
+		$scope.promos=resp.data;
+	},function error(resp){
+		$scope.status = null;
+		if(resp.data.error)
+			$scope.error = resp.data.error;
+		else
+			$scope.error = "Une erreur inconnue est survenue. Veuillez réessayer plus tard.";
+	});
+
+	// Récupération des documents
+	$http({
+		method: "GET",
+		url: "documents/",
+		timeout: 5000
+	}).then(function success(resp){
+		$scope.documents=resp.data;
+	},function error(resp){
+		$scope.status = null;
+		if(resp.data.error)
+			$scope.error = resp.data.error;
+		else
+			$scope.error = "Une erreur inconnue est survenue. Veuillez réessayer plus tard.";
+	});
+
+	// Fonction d'envoi des coord de l'élève
 	$scope.submit_infos = function submit_infos(){
 		$scope.status = "loading";
 		// Si le formulaire n'est pas complet
@@ -44,26 +84,5 @@ function EleveCtrl($scope, $rootScope, $http, $cookies, $location) {
 			else
 				$scope.error = "Une erreur inconnue est survenue. Veuillez réessayer plus tard.";
 		});
-	}
-	$scope.get_documents = function get_documents(){
-
-			$http({
-			method: "GET",
-			data: $scope.eleve,
-			url: "eleve/" + $cookies.id_eleve,
-			timeout: 5000
-		}).then(function success(resp){
-			document.body.classList.add("step-2");
-			document.body.classList.remove("step-1");
-			$scope.status = null;
-
-		},function error(resp){
-			$scope.status = null;
-			if(resp.data.error)
-				$scope.error = resp.data.error;
-			else
-				$scope.error = "Une erreur inconnue est survenue. Veuillez réessayer plus tard.";
-		});
-
 	}
 }
