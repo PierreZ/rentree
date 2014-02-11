36d712458e8f3160d2dd4e702ce5114dfa20624a
PierreZ <pierre.zemb.isen@gmail.com>
2014-02-11 23:28:59 +0100

    adding options to go directly to pane-2

--- js/user.js
+++ js/user.js
@@ -9,13 +9,17 @@ function EleveCtrl($scope, $rootScope, $http, $cookies, $location) {
 		url: "eleve/" + $cookies.id_eleve
 	}).then(function success(resp){
 		$scope.eleve = resp.data;
-		$scope.bodyClass = "step-1 panes";
+		// Si le mail des parents existent, alors on peut passer directement à pane-2
+		if ($scope.eleve.emailparent.length < 1)
+			$scope.bodyClass = "step-1 panes";
+		else
+			$scope.bodyClass = "step-2 panes";
 	}, function error(resp){
 		if(resp.status == 403 && $rootScope.session)
 			$scope.eleve = { email: $rootScope.session.email };
 		$scope.bodyClass = "step-0 panes";
 	});
-	
+
 	$scope.submit_infos = function submit_infos(){
 		$scope.status = "loading";
 		// Si le formulaire n'est pas complet
