11b11ef635a34c225ea701a6a3bc22ad961c5bac
Corentin Delcourt <codl@codl.fr>
2014-02-12 19:27:05 +0100

    fix user page

--- js/user.js
+++ js/user.js
@@ -3,17 +3,30 @@ function EleveCtrl($scope, $rootScope, $http, $cookies, $location) {
 	$scope.bodyClass = "step-0 panes fade";
 	$scope.status = null;
 	$scope.error = null;
-	$scope.get_documents();
-	$http({
-		method: "GET",
-		url: "eleve/" + $cookies.id_eleve
-	}).then(function success(resp){
-		$scope.eleve = resp.data;
-		$scope.bodyClass = "step-2 panes";
+	if($cookies.id_eleve)
+		$http({
+			method: "GET",
+			url: "eleve/" + $cookies.id_eleve
+		}).then(function success(resp){
+			$scope.eleve = resp.data;
+			$cookies.id_eleve = resp.data.id;
+			$scope.bodyClass = "step-2 panes";
+		}, function error(resp){
+			if(resp.status == 403 && $rootScope.session)
+				$scope.eleve = { email: $rootScope.session.email };
+			$scope.bodyClass = "step-0 panes";
+		});
+	else
+		$scope.bodyClass = "step-0 panes"
+
+	$http({ method: "GET", url: "promos/" })
+	.then(function success(resp){
+		$scope.promos = resp.data;
 	}, function error(resp){
-		if(resp.status == 403 && $rootScope.session)
-			$scope.eleve = { email: $rootScope.session.email };
-		$scope.bodyClass = "step-0 panes";
+		if(resp.data && resp.data.error)
+			$scope.error=resp.data.error;
+		else
+			$scope.error="Une erreur est survenue. Veuillez réessayer ultérieurement.";
 	});
 
 	$scope.submit_infos = function submit_infos(){
@@ -28,15 +41,14 @@ function EleveCtrl($scope, $rootScope, $http, $cookies, $location) {
 					return;
 				};
 		$http({
-			method: "PUT",
+			method: $scope.eleve.id?"PUT":"POST",
 			data: $scope.eleve,
-			url: "eleve/" + $cookies.id_eleve,
+			url: "eleve/" + ($scope.eleve.id?$scope.eleve.id:""),
 			timeout: 5000
 		}).then(function success(resp){
 			document.body.classList.add("step-2");
 			document.body.classList.remove("step-1");
 			$scope.status = null;
-
 		},function error(resp){
 			$scope.status = null;
 			if(resp.data.error)
@@ -45,25 +57,5 @@ function EleveCtrl($scope, $rootScope, $http, $cookies, $location) {
 				$scope.error = "Une erreur inconnue est survenue. Veuillez réessayer plus tard.";
 		});
 	}
-	$scope.get_documents = function get_documents(){
 
-			$http({
-			method: "GET",
-			data: $scope.eleve,
-			url: "eleve/" + $cookies.id_eleve,
-			timeout: 5000
-		}).then(function success(resp){
-			document.body.classList.add("step-2");
-			document.body.classList.remove("step-1");
-			$scope.status = null;
-
-		},function error(resp){
-			$scope.status = null;
-			if(resp.data.error)
-				$scope.error = resp.data.error;
-			else
-				$scope.error = "Une erreur inconnue est survenue. Veuillez réessayer plus tard.";
-		});
-
-	}
 }
